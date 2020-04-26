a4b0f8c6111c78925a64ae3e6664157ef4f89711
yavaka <yavakaa@gmail.com>
2020-04-26 20:34:45 +0100

    Internet Control Message Protocol (ICMP) check: Add ICMPHealthCheck class with hardcoded values.

---
+++ HealthCheck/HealthCheck/ICMPHealthCheck.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthCheck
+{
+    public class ICMPHealthCheck : IHealthCheck
+    {
+		private string Host = "www.doesnotexist.com";
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+			try
+			{
+				using (var ping = new Ping())
+				{
+					var reply = await ping.SendPingAsync(Host);
+					switch (reply.Status)
+					{
+						case IPStatus.Success:
+							return (reply.RoundtripTime > Timeout.Infinite)
+								? HealthCheckResult.Degraded()
+								: HealthCheckResult.Healthy();
+						
+								default:
+                            return HealthCheckResult.Unhealthy();
+					}
+				}
+			}
+			catch (Exception)
+			{
+                return HealthCheckResult.Unhealthy();
+			}
+        }
+    }
+}
